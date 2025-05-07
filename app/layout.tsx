c394883bbf888de79644dc8689dc6fe6035e7ca8
Alex <inema9886@gmail.com>
2025-05-07 13:59:59 +0100

    UI matched to figma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
-// app/layout.tsx
 import { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthProvider";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer";
-import Maintainance from "@/components/Maintainance"; // ✅ Using it as a component
-
 export const metadata: Metadata = {
   title: "Lets Help",
   description: "Helping Palestine and Gaza refugees",
 };
 
-const isMaintenance = true; // ⬅️ Toggle ON or OFF
+
 
 export default function RootLayout({
   children,
@@ -21,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {isMaintenance ? (
-          <Maintainance />
-        ) : (
+        
           <AuthProvider>
             <div className="relative z-[1000]">
               <Navbar />
@@ -31,7 +26,7 @@ export default function RootLayout({
             {children}
             <Footer />
           </AuthProvider>
-        )}
+
       </body>
     </html>
   );
